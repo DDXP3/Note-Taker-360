060e486255d605f23ecfd9f26c52e8c9953a2977
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-11-10 00:23:12 -0600

    osh

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const notDat = require('./Develop/db/db.json');
+const notDat = require('./db/db.json');
 const path = require('path');
 const uuid = require('./helpers/uuid');
 const fs = require('fs');
